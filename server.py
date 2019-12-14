f69e5aa999fd4af1750e157d6aeb60f7ceb9240d
jackchengrock <jackchengrock@gmail.com>
2019-12-15 03:31:01 +0800

    Update server.py

--- server.py
+++ server.py
@@ -3,12 +3,16 @@ import random
 
 class RequestHandler_httpd(BaseHTTPRequestHandler):
 	def do_GET(self):
-		messagetosend = bytes((str((random.randint(1,100)))), "utf")
+		global Request
+		messagetosend = bytes('test', "utf")
 		self.send_response(200)
 		self.send_header('Content-Type', 'text/plain')
 		self.send_header('Content-Length', len(messagetosend))
 		self.end_headers()
 		self.wfile.write(messagetosend)
+		Request = self.requestline
+		Request = Request[5: int(len(Request)-9)]
+		print(Request)
 		return
 
 server_address_httpd = ('192.168.66.19', 8080)
