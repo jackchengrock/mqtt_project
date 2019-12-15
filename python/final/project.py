c5685960d9bc4c5ff8456784f63752946e919edd
jackchengrock <jackchengrock@gmail.com>
2019-12-15 20:39:38 +0800

    1

--- python/final/project.py
+++ python/final/project.py
@@ -16,14 +16,6 @@ import traceback
 from io import BytesIO
 
 logging.basicConfig(level=logging.DEBUG)
-<<<<<<< HEAD
-img_src1 = 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/11214290_979728228728313_6464220580933024886_n.jpg?_nc_cat=105&_nc_eui2=AeExGUqRxcZU4upTetprXJyv_Op4AKU9Oi0KerZLpKGeJ1eNelyKE8Va-EMuyfLeMAnEZ1jPtl2Q3kfCGkqxbtXdZlUhJaJEKiF-E1SavOs0xQ&_nc_ohc=k0ZZmaA6kdEAQkCfmXYSTOBPC0T1JsPb1y8boEcDjKwqiECZRTPWLAivA&_nc_ht=scontent-tpe1-1.xx&oh=f1bcf4ad88a887d8ffa735ea151bdeec&oe=5E3F278B'
-img_src2 = 'https://i.ibb.co/VJnjQhF/123.png'
-response1 = req.get(img_src1)
-response2 = req.get(img_src2)
-image_head = Image.open(BytesIO(response1.content))
-image_QRcode = Image.open(BytesIO(response2.content))
-=======
 
 img_src1 = 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/11214290_979728228728313_6464220580933024886_n.jpg?_nc_cat=105&_nc_eui2=AeExGUqRxcZU4upTetprXJyv_Op4AKU9Oi0KerZLpKGeJ1eNelyKE8Va-EMuyfLeMAnEZ1jPtl2Q3kfCGkqxbtXdZlUhJaJEKiF-E1SavOs0xQ&_nc_ohc=k0ZZmaA6kdEAQkCfmXYSTOBPC0T1JsPb1y8boEcDjKwqiECZRTPWLAivA&_nc_ht=scontent-tpe1-1.xx&oh=f1bcf4ad88a887d8ffa735ea151bdeec&oe=5E3F278B'
 img_src2 = 'https://i.ibb.co/VJnjQhF/123.png'
@@ -34,18 +26,10 @@ response2 = req.get(img_src2)
 image_head = Image.open(BytesIO(response1.content))
 image_QRcode = Image.open(BytesIO(response2.content))
 
->>>>>>> parent of 75f8292... ㄇ
 x_head = 319
 y_head = 283
 x_QRcode = 150
 y_QRcode = 150
-<<<<<<< HEAD
-image_head = image_head.resize((x_head,y_head), Image.ANTIALIAS)
-image_QRcode = image_QRcode.resize((x_QRcode,y_QRcode), Image.ANTIALIAS)
-image_head = image_head.resize((x_head,y_head),Image.ANTIALIAS)
-image_QRcode = image_QRcode.resize((x_QRcode,y_QRcode),Image.ANTIALIAS)
-strftime("%Y-%m-%d", gmtime())
-=======
 
 image_head = image_head.resize((x_head,y_head), Image.ANTIALIAS)
 image_QRcode = image_QRcode.resize((x_QRcode,y_QRcode), Image.ANTIALIAS)
@@ -55,82 +39,47 @@ image_QRcode = image_QRcode.resize((x_QRcode,y_QRcode),Image.ANTIALIAS)
 
 strftime("%Y-%m-%d", gmtime())
 
->>>>>>> parent of 75f8292... ㄇ
 try:
 	epd = epd7in5bc.EPD()
 	epd.init()
 	print("Clear")
 	epd.Clear()
-<<<<<<< HEAD
-	#Drawing on the Horizontal image
-	HBlackimage = Image.new('1', (epd.width, epd.height), 255)
-	HRedimage = Image.new('1', (epd.width, epd.height), 255)
-=======
 
 	#Drawing on the Horizontal image
 	HBlackimage = Image.new('1', (epd.width, epd.height), 255)
 	HRedimage = Image.new('1', (epd.width, epd.height), 255)
 
->>>>>>> parent of 75f8292... ㄇ
 	#horizontal
 	print("Drawing")
 	drawblack = ImageDraw.Draw(HBlackimage)
 	drawred = ImageDraw.Draw(HRedimage)
 	font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 75f8292... ㄇ
 	print("Drawline")
 	drawblack.line(((0,0), (0,383)), fill=0, width=5)
 	drawblack.line(((0,0), (639,0)), fill=0, width=5)
 	drawblack.line(((639,383), (0,383)), fill=0, width=5)
 	drawblack.line(((639,0), (639,383)), fill=0, width=5)
-<<<<<<< HEAD
-	drawblack.line(((319,0), (319,383)), fill=0, width=5)
-	drawblack.line(((0,283), (319,283)), fill=0, width=5)
-	drawblack.line(((319,233), (639,233)), fill=0, width=5)
-=======
 
 	drawblack.line(((319,0), (319,383)), fill=0, width=5)
 	drawblack.line(((0,283), (319,283)), fill=0, width=5)
 	drawblack.line(((319,233), (639,233)), fill=0, width=5)
 
->>>>>>> parent of 75f8292... ㄇ
 	#headpic
 	print("head") 
 	newimage = Image.open(os.path.join(picdir, '100x100.bmp'))
 	HBlackimage.paste(image_head, (0,0))
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 75f8292... ㄇ
 	#data
 	print("date")
 	font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
 	drawblack.text((70, 323), "strftime", font=font18, fill=0)
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 75f8292... ㄇ
 	#state
 	print("state")
 	drawblack.text((339,20), 'state', font=font18, fill=0)
 	HBlackimage.paste(newimage, (399,70))
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 75f8292... ㄇ
 	#QRCODE
 	print("QRCODE")
 	drawblack.text((339,263), u'聯絡老師', font=font18, fill=0)
 	HBlackimage.paste(image_QRcode, (450,250))
-<<<<<<< HEAD
-	epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
-	time.sleep(2)
-	epd.sleep()
-		
-=======
 
 	epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
 
@@ -138,7 +87,6 @@ try:
 	epd.sleep()
 
         
->>>>>>> parent of 75f8292... ㄇ
 except IOError as e:
 	logging.info(e)
 	
