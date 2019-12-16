e49db7f3cbe6b421d12a20eedd58e0d722e10610
jackchengrock <jackchengrock@gmail.com>
2019-12-17 04:20:34 +0800

    ㄉ

--- python/final/server.py
+++ python/final/server.py
@@ -2,7 +2,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import random
 import os
 from test import test
-
+from project import draw_display
+from split_image import Image_split
 
 class RequestHandler_httpd(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -15,47 +16,45 @@ class RequestHandler_httpd(BaseHTTPRequestHandler):
 		self.wfile.write(messagetosend)
 		Request = self.requestline
 		Request = Request[5: int(len(Request)-9)]
-
-		test()
 		
-
 		if Request[0:2] == 's1':
 			data = Request[2:]
 			case = 1
 			print(Request[0:2])
 			print(Request[2:])
-			
+			split_image(data)
+			draw_display(data, case)
 		if Request[0:2] == 's2':
 			print(Request[0:2])
 			print(Request[2:])
 			data = Request[2:]
 			case = 2
-
+			draw_display(data, case)
 		if Request[0:6] == 'state1':
 			print(Request[0:6])
 			data = 0
 			case = 3
-
+			draw_display(data, case)
 		if Request[0:6] == 'state2':
 			print(Request[0:6])
 			data = 0
 			case = 4
-
+			draw_display(data, case)
 		if Request[0:6] == 'state3':
 			print(Request[0:6])
 			data = 0
 			case = 5
-
+			draw_display(data, case)
 		if Request[0:6] == 'state4':
 			print(Request[0:6])
 			data = 0
 			case = 6
-
+			draw_display(data, case)
 		if Request[0:6] == 'state5':
 			print(Request[0:6])
 			data = 0
 			case = 7
-
+			draw_display(data, case)
 		return
 
 if __name__ == '__main__':
