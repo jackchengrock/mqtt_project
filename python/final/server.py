aa847604abf4ce85ca015eebc4ac4f1c2e4bebb7
jackchengrock <jackchengrock@gmail.com>
2019-12-18 01:24:13 +0800

    Update server.py

--- python/final/server.py
+++ python/final/server.py
@@ -25,10 +25,8 @@ def on_message(client, userdata, msg):
 		print("Receive msg #2")
 		b = msg.topic + "" + str(msg.payload)
 		print(b)
-		
-	while True:
-    	if now_time.date().year == 2019:
-       		print('123')
+    if now_time.date().year == 2019:
+       	print('123')
 
 	
 
