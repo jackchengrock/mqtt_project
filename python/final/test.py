0777434540ed63538f002ee045005cc88a5da88d
jackchengrock <jackchengrock@gmail.com>
2019-12-17 23:30:21 +0800

    ˇ

--- python/final/test.py
+++ python/final/test.py
@@ -1,4 +1,5 @@
 import paho.mqtt.publish as publish
 
 publish.single("CoreElectronics/test", "Hello", hostname="192.168.66.19")
+publish.single("CoreElectronics/topic", "World!", hostname="192.168.66.19")
 print("Done")
---
+++ python/final/test2.py
@@ -0,0 +1,6 @@
+from test1 import  abc
+
+a = "123"
+b = "123"
+
+abc(a, b)
