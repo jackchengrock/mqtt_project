1b917bb0d0bd05b3919a407f7781ac9df4ae55de
jackchengrock <jackchengrock@gmail.com>
2020-01-13 15:02:39 +0800

    Update split_image.py

--- python/image_preprocessing/split_image.py
+++ python/image_preprocessing/split_image.py
@@ -1,7 +1,7 @@
 import cv2 
 import numpy as np
 from matplotlib import pyplot as plt
-
+import urllib
 import requests as req
 
 img_src = "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/11214290_979728228728313_6464220580933024886_n.jpg?_nc_cat=105&_nc_eui2=AeExGUqRxcZU4upTetprXJyv_Op4AKU9Oi0KerZLpKGeJ1eNelyKE8Va-EMuyfLeMAnEZ1jPtl2Q3kfCGkqxbtXdZlUhJaJEKiF-E1SavOs0xQ&_nc_ohc=k0ZZmaA6kdEAQkCfmXYSTOBPC0T1JsPb1y8boEcDjKwqiECZRTPWLAivA&_nc_ht=scontent-tpe1-1.xx&oh=f1bcf4ad88a887d8ffa735ea151bdeec&oe=5E3F278B"
