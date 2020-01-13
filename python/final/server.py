71ec1d16a23a8cd113dcc6f43fbcd39d36fc68f0
jackchengrock <jackchengrock@gmail.com>
2020-01-13 16:50:59 +0800

    Update server.py

--- python/final/server.py
+++ python/final/server.py
@@ -4,6 +4,7 @@ import paho.mqtt.client as mqtt
 import datetime
 import threading
 import time
+from apscheduler.schedulers.gevent import GeventScheduler
 
 def on_connect(clien, userdata, flags, rc):
 	print("connected with" + str(rc))
@@ -20,22 +21,19 @@ def on_message(client, userdata, msg):
 	if msg.payload == "World!":
 		print("123")
 
-	
-if __name__ == '__main__':
-	def job():
-		client = mqtt.Client()
-		client.on_connect = on_connect
-		client.on_message = on_message
-		client.connect("192.168.66.19", 1883, 60)
-		client.loop_forever()
-	def next():
-		while True:
-			time.sleep(1)
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("192.168.66.19", 1883, 60)
+client.loop_forever()
 
-	connect = threading.Thread(target=job)
-	nextday = threading.Thread(target=next)
-	connect.start()
-	nextday.start()
+def job():
+	print("123")
+
+if __name__ == '__main__':
+	scheduler = GeventScheduler()
+	scheduler.add_job(job, 'interval', seconds=1)
+	scheduler.start()
 	
 	
 	
