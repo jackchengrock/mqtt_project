0215e0417ec2f758387aeab4c14b10dfb396ed76
jackchengrock <jackchengrock@gmail.com>
2020-01-13 15:06:42 +0800

    Update split_image.py

--- python/image_preprocessing/split_image.py
+++ python/image_preprocessing/split_image.py
@@ -7,6 +7,7 @@ import requests as req
 img_src = "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/11214290_979728228728313_6464220580933024886_n.jpg?_nc_cat=105&_nc_eui2=AeExGUqRxcZU4upTetprXJyv_Op4AKU9Oi0KerZLpKGeJ1eNelyKE8Va-EMuyfLeMAnEZ1jPtl2Q3kfCGkqxbtXdZlUhJaJEKiF-E1SavOs0xQ&_nc_ohc=k0ZZmaA6kdEAQkCfmXYSTOBPC0T1JsPb1y8boEcDjKwqiECZRTPWLAivA&_nc_ht=scontent-tpe1-1.xx&oh=f1bcf4ad88a887d8ffa735ea151bdeec&oe=5E3F278B"
 resp = urllib.request.urlopen(img_src)
 img = np.asarray(bytearray(resp.read()), dtype="uint8")
+img = cv2.imdecode(img, cv2.IMREAD_COLOR)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 hist = cv2.calcHist([gray],[0],None,[256],[0,256])
 colors = np.where(hist>5000)
